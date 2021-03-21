1a2f71b7391599d7083d88084aac19fa725959b0
ryuki7 <ryuki111209@i.softbank.jp>
2021-03-21 21:06:48 +0900

    3 finfish

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -32,10 +32,16 @@ RSpec.describe "Tasks", type: :system do
         example 'タスクの新規作成が成功する' do
           visit new_task_path
           fill_in 'Title', with: 'test-title'
+          fill_in 'Content', with: 'test-content'
           select 'todo', from: 'Status'
+          fill_in 'Deadline', with: DateTime.new(2021, 2, 15, 7, 50)
           click_button 'Create Task'
           new_task = user.tasks.find(1)
           expect(page).to have_content 'Task was successfully created.'
+          expect(page).to have_content 'Title: test-title'
+          expect(page).to have_content 'Content: test-content'
+          expect(page).to have_content 'Status: todo'
+          expect(page).to have_content 'Deadline: 2021/2/15 7:50'
           expect(current_path).to eq task_path(new_task)
         end
       end
@@ -44,10 +50,12 @@ RSpec.describe "Tasks", type: :system do
         example 'タスクの新規作成が失敗する' do
           visit new_task_path
           fill_in 'Title', with: nil
+          fill_in 'Content', with: 'test-content'
           select 'todo', from: 'Status'
           click_button 'Create Task'
           expect(page).to have_content '1 error prohibited this task from being saved'
           expect(page).to have_content "Title can't be blank"
+          expect(page).not_to have_content 'Content: test-content'
           expect(current_path).to eq tasks_path
         end
       end
@@ -57,10 +65,12 @@ RSpec.describe "Tasks", type: :system do
           exist_title_task = create(:task)
           visit new_task_path
           fill_in 'Title', with: exist_title_task.title
+          fill_in 'Content', with: 'test-content'
           select 'todo', from: 'Status'
           click_button 'Create Task'
           expect(page).to have_content '1 error prohibited this task from being saved'
           expect(page).to have_content 'Title has already been taken'
+          expect(page).not_to have_content 'Content: test-content'
           expect(current_path).to eq tasks_path
         end
       end
@@ -70,10 +80,16 @@ RSpec.describe "Tasks", type: :system do
       context 'フォームの入力値が正常' do
         example 'タスクの編集が成功する' do
           visit edit_task_path(task)
-          fill_in 'Title', with: 'edit_title'
+          fill_in 'Title', with: 'update-title'
+          fill_in 'Content', with: 'update-content'
           select 'doing', from: 'Status'
+          fill_in 'Deadline', with: DateTime.new(2021, 2, 14, 10, 30)
           click_button 'Update Task'
           expect(page).to have_content 'Task was successfully updated.'
+          expect(page).to have_content 'Title: update-title'
+          expect(page).to have_content 'Content: update-content'
+          expect(page).to have_content 'Status: doing'
+          expect(page).to have_content 'Deadline: 2021/2/14 10:30'
           expect(current_path).to eq task_path(task)
         end
       end
@@ -86,6 +102,7 @@ RSpec.describe "Tasks", type: :system do
           click_button 'Update Task'
           expect(page).to have_content '1 error prohibited this task from being saved'
           expect(page).to have_content "Title can't be blank"
+          expect(page).not_to have_content 'Status: doing'
           expect(current_path).to eq task_path(task)
         end
       end
@@ -99,6 +116,7 @@ RSpec.describe "Tasks", type: :system do
           click_button 'Update Task'
           expect(page).to have_content '1 error prohibited this task from being saved'
           expect(page).to have_content 'Title has already been taken'
+          expect(page).not_to have_content "Title: #{exist_title_task.title}"
           expect(current_path).to eq task_path(task)
         end
       end
@@ -122,6 +140,7 @@ RSpec.describe "Tasks", type: :system do
           expect(page.accept_confirm).to eq 'Are you sure?'
           expect(page).to have_content 'Task was successfully destroyed.'
           expect(current_path).to eq tasks_path
+          expect(page).not_to have_content destory_task.title 
         end
       end
     end
